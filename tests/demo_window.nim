8bfd0089bc75bd51d3e0c2a476bb693310f4739d
Jaremy Creechley <creechley@gmail.com>
2022-02-05 13:32:51 -0800

    fixup demo

--- tests/demo_window.nim
+++ tests/demo_window.nim
@@ -2,22 +2,27 @@ import macros
 import imkivy
 
 widget WidgetsBasic:
+  object:
+    show_demo: bool
+    somefloat: float32
+    counter: int
+
   # Simple window
   Window("Widgets"):
     CollapsingHeader("Widgets"):
 
       Text: "This is some useful text."
-      Checkbox("Demo Window", show_demo)
-      Slider("float", somefloat)
+      Checkbox("Demo Window", self.show_demo)
+      Slider("float", self.somefloat)
 
       Horizontal:
         Button("Button"):
           size: (w: 50, h: 20)
-          on_press: counter.inc
+          on_press: self.counter.inc
         Button("Button"):
           size: (w: 0, h: 0)
-          on_press: counter.inc
-        Text("counter = %d", counter)
+          on_press: self.counter.inc
+        Text("counter = %d", self.counter)
 
       Text("Application average %.3f ms/frame (%.1f FPS)",
           1000.0f / igGetIO().framerate, igGetIO().framerate)
--- tests/test2.nim
+++ tests/test2.nim
@@ -5,7 +5,7 @@ import nimgl/[opengl, glfw]
 
 import imkivy
 import example_window
-# import demo_window
+import demo_window
 
 import std/os, std/times, std/monotimes
 
@@ -36,10 +36,20 @@ proc main() =
 
   igStyleColorsCherry()
 
+  var style = igGetStyle()
+  style.framePadding = ImVec2(x: 5, y: 5) 
+  style.cellPadding = ImVec2(x: 7, y: 4) 
+  style.itemSpacing = ImVec2(x: 11, y: 5) 
+  style.itemInnerSpacing = ImVec2(x: 7, y: 2) 
+  style.scrollbarSize = 24.0
+  style.grabMinSize = 24.0
+
   var show_demo: bool = true
   var somefloat: float32 = 0.0f
   var counter: int32 = 0
-  var demoData = ExampleWindowData()
+  
+  var exData = ExampleWindowData()
+  var demoData = WidgetsBasicData()
 
   while not w.windowShouldClose:
     glfwPollEvents()
@@ -51,8 +61,13 @@ proc main() =
     if show_demo:
       igShowDemoWindow(show_demo.addr)
 
-    ExampleWindow(demoData)
-    # WidgetsBasic()
+    Window("Hello, world!"):
+      ExampleWindow(exData)
+      WidgetsBasic(demoData)
+      Button("Button"):
+        size: (50, 20)
+        on_press: 
+          echo "demoData: ", repr(demoData)
 
     igRender()
 
